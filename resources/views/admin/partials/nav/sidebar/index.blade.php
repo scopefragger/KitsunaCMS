453185fcc5c504e8df7d4670e40b2c914c5cd769
Mark Jones <mark@e3creative.co.uk>
2017-01-24 08:33:31 +0000

    Implemented sub navigation in the sidebar

--- resources/views/admin/partials/nav/sidebar/index.blade.php
+++ resources/views/admin/partials/nav/sidebar/index.blade.php
@@ -1,10 +1,29 @@
 <ul class="sidebar-menu">
-    @foreach($builder_nav as $builder)
-        <li class="">
-            <a href="/admin/index/{{$builder}}">
-                <i class="ti-pie-chart"></i>
-                <p>{{$builder}}</p>
-            </a>
-        </li>
+    @foreach($builder_nav as $key => $builder)
+        @if(empty($builder['children']))
+            <li class="">
+                <a href="/admin/index/{{$builder or ''}}">
+                    <i class="ti-pie-chart"></i>
+                    <p>{{$builder or ''}}</p>
+                </a>
+            </li>
+        @else
+            <li class="treeview">
+                <a href="#">
+                    <i class="fa fa-share"></i> <span>{{$key or ''}}</span>
+                    <span class="pull-right-container">
+              <i class="fa fa-angle-left pull-right"></i>
+            </span>
+                </a>
+                <ul class="treeview-menu">
+                    @if(!empty($builder['children']))
+                        @foreach($builder['children'] as $child)
+                            <li><a href="/admin/index/{{$child or ''}}"><i class="fa fa-circle-o"></i>{{$child or ''}}
+                                </a></li>
+                        @endforeach
+                    @endif
+                </ul>
+            </li>
+        @endif
     @endforeach
 </ul>
